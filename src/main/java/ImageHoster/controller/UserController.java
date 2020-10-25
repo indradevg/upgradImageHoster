d60e0b9dcc7386acd1625e0fa265d08f938741f8
indradev gande <jeffgtxjava@gmail.com>
2020-10-26 02:36:55 +0530

    added a check to find the strenth of the password and if strength is not as per the prerequisite then redirect to the same registration page with an error

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -40,7 +40,15 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
+    public String registerUser(User user, Model model) {
+    	String pattern = "(?=.*[a-zA-Z]+)(?=.*[0-9]+)(?=.*[^a-zA-Z0-9]).*";
+        if(!user.getPassword().matches(pattern)) {
+        	String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+        	System.out.println("password is nt correct");
+        	model.addAttribute("passwordTypeError", error);
+        	model.addAttribute("User", user);
+            return "users/registration";
+        }
         userService.registerUser(user);
         return "redirect:/users/login";
     }
