0904507c08cf27f91dd433c666c7c4aceef839a5
indradev gande <jeffgtxjava@gmail.com>
2020-10-26 17:23:08 +0530

    POJO class has been created with basic contructor and getter,setters

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,93 @@
+package ImageHoster.model;
+
+import java.sql.Date;
+
+public class Comment {
+	
+	
+	private Integer id ;
+	
+	
+	private String text;
+	
+	
+	private Date createdDate;
+	
+	
+	private User user;
+	
+	
+	private Image image;
+
+	
+	
+	public Comment() {
+	}
+	
+	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getText() {
+		return text;
+	}
+
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	public Image getImage() {
+		return image;
+	}
+
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", text=" + text + ", createdDate=" + createdDate + ", user=" + user + ", image="
+				+ image + "]";
+	}
+
+
+	
+	
+	
+	
+}
