aa8cef8eecb2419741f02835f53ee553eb68c1b3
indradev gande <jeffgtxjava@gmail.com>
2020-10-26 18:35:54 +0530

    implemented the logic into CommentController

--- src/main/java/ImageHoster/controller/CommentController.java
+++ src/main/java/ImageHoster/controller/CommentController.java
@@ -1,23 +1,34 @@
 package ImageHoster.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ImageHoster.model.Image;
+import ImageHoster.model.User;
+import ImageHoster.service.CommentService;
 import ImageHoster.service.ImageService;
 
 @Controller
 public class CommentController {
 	
+	@Autowired
 	private ImageService imageService;
 	
+	
+	@Autowired
+	private CommentService commentService;
 	
 	@RequestMapping(path="/image/{imageId}/{imageTitle}/comment",method = RequestMethod.POST)
 	public String uploadComments(@RequestParam("comment") String comment, @PathVariable("imageId") Integer imageId, @PathVariable("imageTitle") String imgTitle) {
 		
-		
-		return "";
-	}
+		// need to get the image and user details from the obtained data
+    	User user = this.imageService.findUserByImageId(imageId);
+    	Image image = this.imageService.findImageByImageId(imageId);
+    	this.commentService.uploadComment(image, comment, user);
+    	
+    	return "redirect:/images/"+imageId+"/"+imgTitle;	}
 }
